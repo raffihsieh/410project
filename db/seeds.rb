24a163ced95ce49e5788877524fbf194cc68794a
raphael <raphael@ubuntu.ubuntu-domain>
2013-05-03 13:46:48 -0500

    asdf

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+Ingredients:
+  ingredient = Ingredient.create([{:name => 'chicken', :price => 5.0 }])
